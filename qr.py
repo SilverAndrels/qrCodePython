b8aab81f9816cbd9ebba3fd409110cd061bd0408
SilverAndrels <andresilva64@outlook.com>
2022-02-05 13:21:44 -0300

    Version 0.0.2

--- qr.py
+++ qr.py
@@ -2,5 +2,5 @@ import qrcode
 print("Olá, este é um gerador de qrCode!")
 link = input("Cole aqui o link: ")
 imagem = qrcode.make(link)
-nome = input("Nome do qr e extenção: ")
+nome = input("Nome do qr e extenção:(exemplo: nome.png...) ")
 imagem.save(nome)
