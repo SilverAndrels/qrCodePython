c3db456005382cb29af09d153c9117d6491e7a85
SilverAndrels <andresilva64@outlook.com>
2022-08-05 11:42:55 -0300

    Change the language

--- qr.py
+++ qr.py
@@ -1,6 +1,6 @@
 import qrcode
-print("Olá, este é um gerador de qrCode!")
-link = input("Cole aqui o link: ")
-imagem = qrcode.make(link)
-nome = input("Nome do qr e extenção:(exemplo: nome.png...) ")
-imagem.save(nome)
+print("Hi, this is the QRcode generator using python!")
+link = input('Paste the link: ')
+image_qr = qrcode.make(link)
+name = input("QR name and extension:(example: name.png...) ")
+image_qr.save(name)
